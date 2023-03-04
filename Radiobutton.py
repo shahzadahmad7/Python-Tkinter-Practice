123dabd7cbbcf13e7bf19a8eea1d145a98b8ecb3
Shahzad Ahmad <124117411+shahzadahmad7@users.noreply.github.com>
2023-03-04 02:51:15 -0800

    Add files via upload

--- Radiobutton.py
+++ Radiobutton.py
@@ -6,15 +6,18 @@ root.title("Radiobutton Tutorial ")
 root.geometry("600x450")
 
 def gender():
+	
+	# 
 	tmsg.showinfo("Gender", f"The gender of {a.get()} is {var.get()}")
-var=StringVar()
-var.set(" ")
+
 
 Label(root, text="Name of your friend:", font="arial 10 bold").pack()
 a=Entry (root)
 a.pack()
 Label(root, text="Tell us the gender of your friend...", font="arial 10 bold").pack()
 
+var=StringVar()
+var.set(" ")
 Radiobutton(root, text="Male", variable=var, value="Male").pack()
 
 Radiobutton(root, text="Female", variable=var, value="Female").pack()
